f7596995885aead600a37f7cce88bdb78720e64f
NubleX <nublexer@hotmail.com>
2024-11-14 18:33:45 +0100

    Update models.py
    radar and lidar added

--- src/data_storage/models.py
+++ src/data_storage/models.py
@@ -1,6 +1,6 @@
 # If you haven’t already, define a model in models.py for storing the inductive loop sensor data.
 
-from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy import Column, Integer, String, Float, DateTime
 from .database import Base
 
 class LoopSensorData(Base):
@@ -10,4 +10,14 @@ class LoopSensorData(Base):
     loop_id = Column(String, index=True)
     vehicle_count = Column(Integer)
     occupancy = Column(Integer)
+    timestamp = Column(DateTime)
+    
+class RadarLidarData(Base):
+    __tablename__ = "radar_lidar_data"
+
+    id = Column(Integer, primary_key=True, index=True)
+    sensor_type = Column(String, index=True)  # Either "radar" or "lidar"
+    speed = Column(Float)  # Speed of the detected vehicle
+    distance = Column(Float)  # Distance from the sensor
+    object_type = Column(String)  # Type of object (e.g., "car", "truck")
     timestamp = Column(DateTime)
